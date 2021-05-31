1ed53de3207d5d4d71e78f0216eb42c97a7e4431
dmm20210201 <tnaoki.09j@gmail.com>
2021-05-31 11:42:53 +0000

    [Fix]いいね機能修正

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,17 +1,17 @@
 class FavoritesController < ApplicationController
 
   def create
-    @cafe_image = CafeImage.find(params[:cafe_image_id])
-    favorite = current_user.favorites.new(cafe_image_id: @cafe_image.id)
+    cafe_image = CafeImage.find(params[:id])
+    favorite = current_user.favorites.new(cafe_image_id: cafe_image.id)
     favorite.save
-    redirect_to cafe_images_path(cafe_images)
+    redirect_to cafe_images_path(cafe_image)
   end
 
   def destroy
-    @cafe_image = CafeImage.find(params[:cafe_image_id])
-    favorite = current_user.favorites.find_by(cafe_image_id: @cafe_image.id)
+    cafe_image = CafeImage.find(params[:id])
+    favorite = current_user.favorites.find_by(cafe_image_id: cafe_image.id)
     favorite.destroy
-    redirect_to cafe_images_path(cafe_images)
+    redirect_to cafe_images_path(cafe_image)
   end
 
 end
